9107f2f9bb652e4538057feb5ff33dde1002f6e4
Steve Hodges <steve.hodges@thefutureis.mobi>
2013-05-02 02:27:34 -0400

    Works with ruby OFF rails, and a bugfix

--- lib/talk_like_a_pirate.rb
+++ lib/talk_like_a_pirate.rb
@@ -17,14 +17,22 @@ private #####################################################################
   def self.translate_string(me_string)
     me_string.split(/ /).map do |word|
       capitalized = (word.slice(0,1) == word.slice(0,1).upcase)
-      pluralized = word.pluralize == word
-      if dictionary.has_key?(word.downcase.singularize)
-        word = dictionary[word.downcase.singularize]
-        word = pluralized ? word.pluralize : word
-        word = capitalized ? capitalize_first(word) : word
-      end
-      word
+      word = rails_present? ? piratize_with_pluralization(word) : piratize(word)
+      capitalized ? capitalize_first(word) : word
     end.join(" ")
+  end
+
+  def self.piratize_with_pluralization(word)
+    pluralized = word.pluralize == word
+    if dictionary.has_key?(word.downcase.singularize)
+      word = dictionary[word.downcase.singularize]
+      word = pluralized ? word.pluralize : word
+    end
+    word
+  end
+
+  def self.piratize(word)
+    dictionary.has_key?(word.downcase) ? dictionary[word.downcase] : word
   end
 
   def self.prepare_original_sentence(sentence)
@@ -46,11 +54,11 @@ private #####################################################################
   end
 
   def self.sprinklings_of_flavor
-    @@fill ||= (config["pirate_flavor"] << local_flavor).compact
+    @@fill ||= (config["pirate_flavor"] << local_flavor).compact.flatten
   end
 
   def self.config
-    @@config ||= YAML.load_file(File.dirname(__FILE__) + "/talk_like_a_pirate/pirate_booty.yml")
+    @@config ||= YAML::load_file(File.dirname(__FILE__) + "/talk_like_a_pirate/pirate_booty.yml")
   end
 
   def self.local_dictionary
@@ -62,7 +70,7 @@ private #####################################################################
   end
 
   def self.local_config
-    @@local_configs ||= YAML.load_file("#{Rails.root.to_s}/config/pirate_booty.yml") rescue {}
+    @@local_configs ||= YAML::load_file("#{Rails.root.to_s}/config/pirate_booty.yml") rescue {}
   end
 
   def self.capitalize_first(string)
@@ -71,6 +79,14 @@ private #####################################################################
     string.slice(0,1).capitalize + string.slice(1..-1)
   end
 
+  def self.rails_present?
+    Object.const_defined? :Rails
+  end
+
 end
 
-require 'talk_like_a_pirate/railties'
+if Object.const_defined? :Rails
+  require "talk_like_a_pirate/railties"
+else
+  require "yaml"
+end
